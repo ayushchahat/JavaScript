c38fda85a7e178a98f026f72c496bd1ca023d15b
Ayush Kumar <97400680+ayushchahat@users.noreply.github.com>
2024-06-20 22:39:25 +0000

    Strings

---
+++ Basic/String.js
@@ -0,0 +1,14 @@
+const name= "Ayush";
+const repoCount=100
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const Name="    Ayush     "
+console.log(Name);
+console.log(Name.trim());
+
+const url = "https://Ayush.com/Ayush%20Kumar"
+
+console.log(url.replace('%20','-'));
