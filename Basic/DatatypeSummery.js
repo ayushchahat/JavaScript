8457a651756ddca01abdd8d967ee62d7df3a2a63
Ayush Kumar <97400680+ayushchahat@users.noreply.github.com>
2024-06-20 22:15:31 +0000

    DataType Summery

---
+++ Basic/DatatypeSummery.js
@@ -0,0 +1,22 @@
+// There are two types of datatype:
+// 1.Premitive :- Call by value
+
+// 7 types: String, Number ,Boolean, null, undefined, symbol,BigInt
+
+//2. Non Primitive:- Call by reference
+
+// Array,Objects,Functions
+
+//Array
+const heros=["Dom","Tom","Pom"];
+
+//Object
+let myObj = {
+    name:"Ayush";
+    age:22
+}
+
+//Function
+const Myfun = function(){
+    console.log("Ayush");
+}
