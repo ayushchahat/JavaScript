16a8c3a3a0cace26ec2e7094d29ac5b75e34f1a3
Ayush Kumar <97400680+ayushchahat@users.noreply.github.com>
2024-06-20 21:50:48 +0000

    Operation in javascript

--- Basic/ConversionOperation.js
+++ Basic/ConversionOperation.js
@@ -1,11 +1,11 @@
 const score="33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);//return NaN=>Not a Number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//return NaN=>Not a Number
 
 //"33"=>33
 // "33abc"=>NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber);//return NaN=>Not a Number
 let isLoggedIn=1
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1=> true;0=> false
 //"" => false
@@ -24,5 +24,40 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Important Operations ***************
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+
+// let s1="Ayush"
+// let s2=" Kumar"
+// let s3=s1+s2
+
+// console.log(s3);
+
+// console.log("1" + 2)
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+//  console.log(true)
+//  console.log(+true);
+//  console.log(true+);=>gives error
+//  console.log(+"");
+
+ let n1,n2,n3
+
+ n1=n2=n3= 2 + 2
+
