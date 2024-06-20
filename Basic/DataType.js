bfb91d07709fafce82075281d2a30343843cf6bf
Ayush Kumar <97400680+ayushchahat@users.noreply.github.com>
2024-06-20 20:32:20 +0000

    Const,Let,Var data types in js

---
+++ Basic/DataType.js
@@ -0,0 +1,13 @@
+const studentName="Ayush";
+let studentEmail="ayushchahat@gamail.com";
+var studentenRoll_Number="615165";
+let StudentAddress;
+
+/*
+Prefer Not to use var because of 
+issue in block scope and functional 
+scope
+*/
+
+console.log(studentName);
+console.table([studentName,studentEmail,studentenRoll_Number,StudentAddress]);
