752a2298201ea58627115e463cf166ead2e208ea
Ayush Kumar <97400680+ayushchahat@users.noreply.github.com>
2024-06-20 20:57:09 +0000

    Different Datatypes in javascript

---
+++ Basic/DataType2.js
@@ -0,0 +1,27 @@
+"use strict";// treat all JS code as newer version
+
+// alert (3+3) //we are using nodejs, not browser
+
+console.log(3+3);// code readability should be high
+console.log("Ayush");
+
+//Datatypes in JS
+
+let name="Ayush";
+let age=18;
+let isLoggedIn = false
+let state;
+
+//number=>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//NULL=>return undefined
+// undefined=>value is not assigned
+//symbol=>unique
+
+// object
+
+console.log(typeof undefined);//return undefined
+console.log(typeof null);//return object
